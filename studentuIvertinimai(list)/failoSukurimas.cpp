bdfd942e2fc77d4589ff7d174ad7a06ebbcb8285
Sandra <macsa@LAPTOP-2GDA5VDB>
2020-10-30 18:39:27 +0200

    Sukurta projekto kopija

---
+++ studentuIvertinimai(list)/failoSukurimas.cpp
@@ -0,0 +1,47 @@
+#include "failoSukurimas.h"
+
+void naujasFailas(int n, int kiekPazymiu)
+{
+	string skaicius = to_string(n);
+
+	auto start = std::chrono::high_resolution_clock::now();
+
+	ofstream fr("kursiokai" + skaicius + ".txt");
+
+	fr << left << setw(15) << "Vardas" << setw(20) << "Pavarde";
+	for (int i = 0; i < kiekPazymiu; i++)
+	{
+		string skaicius = to_string(i + 1);
+		fr << setw(5) << "ND" + skaicius;
+	}
+	fr << setw(5) << "Egz" << endl;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		Asmuo asmuo;
+		string skaicius = to_string(i+1);
+
+		asmuo.vardas = "Vardas" + skaicius;
+		asmuo.pavarde = "Pavarde" + skaicius;
+		fr << left << setw(15) << asmuo.vardas << setw(20) << asmuo.pavarde;
+
+		using hrClock = std::chrono::high_resolution_clock;
+		std::mt19937 mt(static_cast<long unsigned int>(hrClock::now().time_since_epoch().count()));
+		std::uniform_int_distribution<int> dist(0, 10);
+
+		for (int j = 0; j < kiekPazymiu; j++)
+		{
+			asmuo.pazymiai.push_back(dist(mt));
+			fr << setw(5) << asmuo.pazymiai.at(j);
+		}
+
+		asmuo.egzaminas = dist(mt);
+		fr << setw(5) << asmuo.egzaminas << endl;
+	}
+
+	fr.close();
+	auto end = std::chrono::high_resolution_clock::now();
+	std::chrono::duration<double> diff = end - start;
+	std::cout << "Failo sukurimas uztruko: " << diff.count() << " s\n";
+}
---
+++ studentuIvertinimai(list)/studentuIvertinimai(list).cpp
@@ -0,0 +1,68 @@
+// studentuIvertinimai.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include "Asmuo.h"
+#include "duomenuNuskaitymas.h"
+#include "duomenuIvedimas.h"
+#include "galutinisPazymys.h"
+#include "duomenuIsvedimas.h"
+#include "duomenuPasirinkimas.h"
+#include "failoSukurimas.h"
+#include "studentuSarasoDalinimas.h"
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+using std::vector;
+
+
+int main()
+{
+    srand(time(0));
+
+    vector <Asmuo> studentai;
+    vector <Asmuo> galvociai;
+    vector <Asmuo> vargsiukai;
+
+    string skaiciavimoBudas;
+    cout << "Pasirinkite, kaip skaiciuoti galutini bala: 'a' - naudojant vidurki; 'b' - naudojant mediana" << endl;
+    cin >> skaiciavimoBudas;
+
+    /*cout << "Failas su 1000 irasu: \n";
+    naujasFailas(1000, 5);
+    duomenuNuskaitymas(studentai, "kursiokai1000.txt");
+    studentuRusiavimas(studentai, galvociai, vargsiukai, skaiciavimoBudas);
+    naujiSarasai(galvociai, vargsiukai, "1000");
+    cout << endl;
+
+    cout << "Failas su 10000 irasu: \n";
+    naujasFailas(10000, 5);
+    duomenuNuskaitymas(studentai, "kursiokai10000.txt");
+    studentuRusiavimas(studentai, galvociai, vargsiukai, skaiciavimoBudas);
+    naujiSarasai(galvociai, vargsiukai, "10000");
+    cout << endl;
+
+    cout << "Failas su 1000000 irasu: \n";
+    naujasFailas(100000, 5);
+    duomenuNuskaitymas(studentai, "kursiokai100000.txt");
+    studentuRusiavimas(studentai, galvociai, vargsiukai, skaiciavimoBudas);
+    naujiSarasai(galvociai, vargsiukai, "100000");
+    cout << endl;
+
+    cout << "Failas su 10000000 irasu: \n";
+    naujasFailas(1000000, 5);
+    duomenuNuskaitymas(studentai, "kursiokai1000000.txt");
+    studentuRusiavimas(studentai, galvociai, vargsiukai, skaiciavimoBudas);
+    naujiSarasai(galvociai, vargsiukai, "1000000");
+    cout << endl;
+
+    cout << "Failas su 100000000 irasu: \n";
+    naujasFailas(10000000, 5);
+    duomenuNuskaitymas(studentai, "kursiokai10000000.txt");
+    studentuRusiavimas(studentai, galvociai, vargsiukai, skaiciavimoBudas);
+    naujiSarasai(galvociai, vargsiukai, "10000000");
+    cout << endl; */
+
+    return 0;
+}
